7f4500380cedb22de6acd2740086a3bc778ed599
Khaliq <khaliqgant@gmail.com>
2021-09-14 22:24:26 +0200

    [titl-24] material ui login components

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -1,29 +1,36 @@
 import React from 'react';
 import Button from "@material-tailwind/react/Button";
+import Input from "@material-tailwind/react/Input";
 
 const Login: React.FC = () => {
   return (
-    <div className="bg-green-400 shadow-md h-screen grid">
-      <div className="rounded px-8 pt-6 pb-8 mb-4 flex flex-col">
-        <div className="mb-4">
-          <label className="block text-grey-darker text-sm font-bold mb-2" htmlFor="username">
-            Username
-          </label>
-          <input className="shadow appearance-none border rounded w-full py-2 px-3 text-grey-darker" id="username" type="text" placeholder="Username" />
-        </div>
-        <div className="mb-6">
-          <label className="block text-grey-darker text-sm font-bold mb-2" htmlFor="password">
-            Password
-          </label>
-          <input className="shadow appearance-none border border-red rounded w-full py-2 px-3 text-grey-darker mb-3" id="password" type="password" placeholder="******************" />
-          <p className="text-red text-xs italic">Please choose a password.</p>
-        </div>
-        <div className="flex items-center justify-between">
-          <Button color="lightGreen" ripple="light">Sign In</Button>
-          <a className="inline-block align-baseline font-bold text-sm text-blue hover:text-blue-darker" href="/">
-            Forgot Password?
-          </a>
-        </div>
+    <div className="w-full flex flex-wrap justify-center">
+      <div className="flex flex-col pt-4">
+        <Input
+          type="text"
+          color="green"
+          size="lg"
+          outline={true}
+          placeholder="Username"
+        />
+        <Input
+          type="password"
+          color="green"
+          size="lg"
+          outline={true}
+          placeholder="Password"
+        />
+        <Button
+          color="green"
+          buttonType="filled"
+          size="regular"
+          rounded={false}
+          block={false}
+          iconOnly={false}
+          ripple="light"
+        >
+            Submit
+        </Button>
       </div>
     </div>
   )
