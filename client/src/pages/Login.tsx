188063bbba8efc31f4fc388f67ad166021b6ebff
Khaliq <khaliqgant@gmail.com>
2021-09-15 19:51:24 +0200

    [titl-35] add TitlLogo and clean up login page

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -1,17 +1,24 @@
 import React from 'react';
 import Button from "@material-tailwind/react/Button";
 import Input from "@material-tailwind/react/Input";
+import TitlLogo from 'assets/logo.svg';
 
 const Login: React.FC = () => {
   return (
-    <div className="w-full flex flex-wrap justify-center">
-      <div className="flex flex-col pt-4">
+    <div className="w-full flex flex-col items-center min-h-screen p-4 text-white sm:p-8 md:p-12">
+      <img
+        src={TitlLogo}
+        alt="Titl"
+        width="140"
+        className="inline-block"
+      />
+      <div className="flex flex-col pt-8 space-y-6 w-full sm:max-w-md">
         <Input
           type="text"
           color="green"
           size="lg"
           outline={true}
-          placeholder="Username"
+          placeholder="Email Address"
         />
         <Input
           type="password"
@@ -28,6 +35,7 @@ const Login: React.FC = () => {
           block={false}
           iconOnly={false}
           ripple="light"
+          className="bg-green"
         >
             Submit
         </Button>
