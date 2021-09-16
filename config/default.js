7e606b47ee5f76c217f93bf354b9ef8acbfcb660
Khaliq <khaliqgant@gmail.com>
2021-09-16 10:08:12 +0200

    [titl-35] allow a cookie to be stored in the browser so requests will go through to the API

--- config/default.js
+++ config/default.js
@@ -4,11 +4,18 @@ module.exports = {
   port: 8000,
   settings: {
     loggerFormat: "tiny",
-    jwt: {
-      secret: Env.get('JWT_SECRET')
-    },
     session: {
       "store": "@foal/typeorm"
-    }
+    },
+    jwt: {
+      secret: Env.get('JWT_SECRET'),
+      cookie: {
+        name: 'titl',
+        domain: Env.get('JWT_COOKIE_DOMAIN'),
+        httpOnly: Env.get('JWT_COOKIE_HTTP_ONLY'),
+        sameSite: "strict",
+        secure: true,
+      }
+    },
   }
 }
