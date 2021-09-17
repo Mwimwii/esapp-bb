7a21203f62372138b3577cab69eba5fb46b01a3c
Khaliq <khaliqgant@gmail.com>
2021-09-17 12:47:26 +0200

    [titl-35] remove login magic

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,5 +1,4 @@
 export { default as Home } from './Home';
 export { default as Login } from './Login';
-export { default as LoginMagic } from './LoginMagic';
 export { default as Signup } from './Signup';
 export { default as Dashboard } from './Dashboard';
