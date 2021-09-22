58e1aecc0f24ba602a315cd19426a7dc34305e3d
Khaliq <khaliqgant@gmail.com>
2021-09-22 15:35:18 +0200

    [titl-47] disable eslint warning

--- src/seeds/AddJotformToken.ts
+++ src/seeds/AddJotformToken.ts
@@ -3,8 +3,8 @@ import { Connection } from 'typeorm'
 import { ApiToken } from '../app/models/ApiToken';
 
 export default class AddJotformToken implements Seeder {
+  // eslint-disable-next-line @typescript-eslint/explicit-member-accessibility
   public async run(_factory: Factory, connection: Connection): Promise<any> {
-
     const token = process.env.JOTFORM_API_KEY || 'mDVE9BVWBm2f8bkcTH';
 
     await connection
