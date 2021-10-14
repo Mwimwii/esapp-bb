64028f12fede544ccf254b6f9743919bd516803d
Khaliq <khaliqgant@gmail.com>
2021-10-14 13:06:02 +0200

    [titl-1201184756283102] add xsrf token

--- config/default.js
+++ config/default.js
@@ -1,21 +1,24 @@
-const { Env } = require('@foal/core');
+const { Env } = require("@foal/core");
 
 module.exports = {
   port: 8000,
   settings: {
     loggerFormat: "tiny",
     session: {
-      "store": "@foal/typeorm"
+      store: "@foal/typeorm",
+      csrf: {
+        enabled: true,
+      },
     },
     jwt: {
-      secret: Env.get('JWT_SECRET'),
+      secret: Env.get("JWT_SECRET"),
       cookie: {
-        name: 'titl',
-        domain: Env.get('JWT_COOKIE_DOMAIN'),
-        httpOnly: Env.get('JWT_COOKIE_HTTP_ONLY'),
+        name: "titl",
+        domain: Env.get("JWT_COOKIE_DOMAIN"),
+        httpOnly: Env.get("JWT_COOKIE_HTTP_ONLY"),
         sameSite: "strict",
         secure: true,
-      }
+      },
     },
-  }
-}
+  },
+};
