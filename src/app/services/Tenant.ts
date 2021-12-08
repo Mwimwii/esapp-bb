373b0e1de6e654f81925ef0b92ab24db0b9dd73e
Ham <lubegah@gmail.com>
2021-12-08 13:00:33 +0300

    Added contact details to contact

--- src/app/services/Tenant.ts
+++ src/app/services/Tenant.ts
@@ -1,10 +1,15 @@
-import path from 'path';
-import { File, Disk } from '@foal/storage';
-import { dependency } from '@foal/core';
+import path from "path";
+import { File, Disk } from "@foal/storage";
+import { dependency } from "@foal/core";
 
-import { OnboardingQuestions } from '@titl-all/shared/dist/types';
-import { Language, ContactType } from '@titl-all/shared/dist/enum';
-import { User, Contact } from 'app/models';
+import { OnboardingQuestions } from "@titl-all/shared/dist/types";
+import {
+  Language,
+  ContactType,
+  ContactDetailType,
+  ContactDetailStatus,
+} from "@titl-all/shared/dist/enum";
+import { User, Contact, ContactDetail } from "app/models";
 
 export class TenantService {
   @dependency
@@ -36,26 +41,54 @@ export class TenantService {
     if (contactType && contactType.length > 0) {
       // format this
       //tenantContact.contactType = contactType[0];
-      tenantContact.contactType = 'Tenant' as ContactType;
+      tenantContact.contactType = "Tenant" as ContactType;
     }
 
     const phoneNumber = this.obtainNonWhatsAppPhoneNumber(
       String(firstPhoneNumber),
       String(firstNumberIsWhatsApp),
       String(secondPhoneNumber),
-      String(secondNumberIsWhatsApp),
+      String(secondNumberIsWhatsApp)
     );
 
     if (picture) {
       tenantContact.hasPicture = true;
       const directoryName = `0_${firstName}_${lastName}`;
-      await this.disk.write(`attachments/contacts/${directoryName}`, picture.buffer, {
-        name: `PROFILE_PORT_${firstName?.toUpperCase()}_${lastName?.toUpperCase()}_${phoneNumber}.${path.extname(String(picture.filename))}`,
-      });
-
+      await this.disk.write(
+        `attachments/contacts/${directoryName}`,
+        picture.buffer,
+        {
+          name: `PROFILE_PORT_${firstName?.toUpperCase()}_${lastName?.toUpperCase()}_${phoneNumber}.${path.extname(
+            String(picture.filename)
+          )}`,
+        }
+      );
     }
-    tenantContact.languages = Array.isArray(languages) ? this.formatLanguages(languages) : [];
+    tenantContact.languages = Array.isArray(languages)
+      ? this.formatLanguages(languages)
+      : [];
     tenantContact.createdBy = user;
+
+    const createdContactDetailFirstPhoneNum = new ContactDetail();
+    createdContactDetailFirstPhoneNum.contactDetailType =
+      firstNumberIsWhatsApp === "Yes"
+        ? ContactDetailType.whatsapp
+        : ContactDetailType.phone;
+    createdContactDetailFirstPhoneNum.contactDetailValue =
+      String(firstPhoneNumber);
+    createdContactDetailFirstPhoneNum.status = ContactDetailStatus.active;
+
+    const createdContactDetailSecondPhoneNum = new ContactDetail();
+    createdContactDetailSecondPhoneNum.contactDetailType =
+      secondNumberIsWhatsApp === "Yes"
+        ? ContactDetailType.whatsapp
+        : ContactDetailType.phone;
+    createdContactDetailSecondPhoneNum.contactDetailValue =
+      String(secondPhoneNumber);
+    createdContactDetailSecondPhoneNum.status = ContactDetailStatus.active;
+
+    createdContactDetailFirstPhoneNum.contact = tenantContact;
+    createdContactDetailSecondPhoneNum.contact = tenantContact;
 
     tenantContact.save();
 
@@ -66,29 +99,30 @@ export class TenantService {
     firstNumber: string,
     firstIsWhatsApp: string,
     secondNumber: string,
-    secondIsWhatsApp: string) {
-      if (firstIsWhatsApp === 'No') {
-        return firstNumber;
-      }
-
-      if (secondIsWhatsApp === 'No') {
-        return secondNumber;
-      }
-
+    secondIsWhatsApp: string
+  ) {
+    if (firstIsWhatsApp === "No") {
       return firstNumber;
+    }
+
+    if (secondIsWhatsApp === "No") {
+      return secondNumber;
+    }
+
+    return firstNumber;
   }
 
   formatLanguages(languages?: string[]): Language[] {
-    const formattedLanguages = languages?.map(language => {
-      switch(language) {
-        case 'English':
+    const formattedLanguages = languages?.map((language) => {
+      switch (language) {
+        case "English":
           return Language.English;
-        case 'Luganda':
+        case "Luganda":
           return Language.Luganda;
-        case 'Swahali':
+        case "Swahali":
           return Language.Swahali;
         default:
-          return '';
+          return "";
       }
     });
 
