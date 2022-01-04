5f066a854e3d4ecdf9b37ec3dad874a888e8da05
Isaac Obote <iobote@udbl.co.ug>
2022-01-04 05:41:44 +0300

    [titl-1201604347302892] removed functions from app/utils

--- src/app/utils/convertToLabel.ts
+++
@@ -1,8 +0,0 @@
-import { toPascalCase } from './toPascalCase';
-
-export function convertToLabel(text: string) {
-  if (!text)
-    return;
-  text = toPascalCase(text.replace(/[^a-zA-Z0-9.]/g, ' '));
-  return text;
-}
