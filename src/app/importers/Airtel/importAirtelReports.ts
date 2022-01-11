bdd9dbb0ecc8b7c87196a4b3920f8993135315c8
Isaac Obote <iobote@udbl.co.ug>
2022-01-12 02:50:56 +0300

    [titl-1201629851552237] Resolved Airtel Dependencies

--- src/app/importers/Airtel/importAirtelReports.ts
+++ src/app/importers/Airtel/importAirtelReports.ts
@@ -1,4 +1,3 @@
-import { S3Client } from '@aws-sdk/client-s3';
 import { env } from 'process';
 import { EntityManager } from 'typeorm';
 import { updateRelations } from '../airtable/updateRelations';
@@ -7,19 +6,19 @@ import { readAirtelCsvFile } from './readAirtelCsvFile';
 const AWS = require('aws-sdk');
 const s3 = new AWS.S3({ apiVersion: '2006-03-01' });
 
-export function importAirtelReports(manager: EntityManager, s3Client: S3Client) {
+export function importAirtelReports(manager: EntityManager) {
   s3.listObjectsV2({
     Bucket: env.AWS_BUCKET,
     Prefix: 'reports/airtel/' // Can be your folder name
-  }, function (err, data) {
+  }, function (err: { stack: any; }, data: { Contents: any[]; }) {
     if (err)
       console.log(err, err.stack); // an error occurred
     else {
       console.log(data.Contents);
-      data.Contents.forEach(obj => {
+      data.Contents.forEach((obj: { Key: string; }) => {
 
         if (obj.Key.startsWith('reports/airtel/')) {
-          readAirtelCsvFile(manager, s3Client, obj);
+          readAirtelCsvFile(manager, obj);
         }
       });
       updateRelations(manager);
--- src/app/importers/Airtel/readAirtelCsvFile.ts
+++ src/app/importers/Airtel/readAirtelCsvFile.ts
@@ -1,4 +1,3 @@
-import { S3Client } from '@aws-sdk/client-s3';
 import { env } from 'process';
 import { EntityManager } from 'typeorm';
 import { Payment } from '../../models';
@@ -9,13 +8,13 @@ import readline = require('readline');
 import AWS = require('aws-sdk');
 const s3 = new AWS.S3({ apiVersion: '2006-03-01' });
 
-export function readAirtelCsvFile(manager: EntityManager, s3Client: S3Client, obj: any) {
+export function readAirtelCsvFile(manager: EntityManager, obj: any) {
   console.log(obj);
   const paymentRepo = manager.getRepository(Payment);
   const rl = readline.createInterface({
     input: s3.getObject({
-      Bucket: env.AWS_BUCKET,
-      Key: obj.key || obj.Key
+      Bucket: env.AWS_BUCKET || 'titl',
+      Key: obj.Key || obj.key
     }).createReadStream()
   });
 
