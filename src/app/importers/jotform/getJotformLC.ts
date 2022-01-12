21535d05bcd0e9cfee2439c1e50eaa3a20892522
Isaac Obote <iobote@udbl.co.ug>
2022-01-12 05:07:07 +0300

    [titl-1201629851552237] getJotformLC fix eslint

--- src/app/importers/jotform/getJotformLC.ts
+++ src/app/importers/jotform/getJotformLC.ts
@@ -12,19 +12,19 @@ export async function getJotformLC(record: any, repository: Repository<Contact>)
   let contact = await repository.findOne({ where: [{ firstName: readJotFormValue(record, 190, 1), lastName: readJotFormValue(record, 190, 2) }] });
 
   if (!contact) {
-    contact = <Contact><unknown>({
+    contact = ({
       firstName: readJotFormValue(record, 190, 1),
       lastName: readJotFormValue(record, 190, 2),
       contactType: ContactType.govtrepresentative,
       contactDetails: []
-    });
+    } as unknown) as Contact;
     if (SanitizeNumber(readJotFormValue(record, 190, 3))) {
       contact.contactDetails.push(
-        <ContactDetail>({
+        {
           contactDetailType: ContactDetailType.phone,
           preferred: true,
           contactDetailValue: SanitizeNumber(readJotFormValue(record, 190, 3))
-        })
+        } as ContactDetail
       );
     }
     await repository.save(contact);
