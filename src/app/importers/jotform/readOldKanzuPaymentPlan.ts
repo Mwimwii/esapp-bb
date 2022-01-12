6d59037cc463c4ca2971385b2bd18300a1652de3
Isaac Obote <iobote@udbl.co.ug>
2022-01-12 03:39:58 +0300

    [titl-1201629851552237] readOldKanzuPaymentPlan Resolved return type and initilization

--- src/app/importers/jotform/readOldKanzuPaymentPlan.ts
+++ src/app/importers/jotform/readOldKanzuPaymentPlan.ts
@@ -10,28 +10,29 @@ import {
 } from '@titl-all/shared/dist/enum';
 
 export function readOldKanzuPaymentPlan(record: any, agreement: Agreement): PaymentPlan {
-    if (readJotFormValue(record, 184, null)) {
-        const kanzuPaymentPlan = <PaymentPlan>({
-            paymentType: PaymentType.kanzu,
-            baseAmount: readJotFormValue(record, 184, null) || 0,
-            agreedAmount: readJotFormValue(record, 184, null) || 0,
-            effectiveDate: agreement.dateArrived || record.created_at,
-            cycle: PaymentCycle.oneoff,
-            currency: PaymentCurrency.ugx,
-            status: (readJotFormValue(record, 24, null) === 'YES') ? PaymentPlanStatus.completed : PaymentPlanStatus.active,
-            payments: []
-        });
+  if (readJotFormValue(record, 184, null)) {
+    const kanzuPaymentPlan = <PaymentPlan><unknown>({
+      paymentType: PaymentType.kanzu,
+      baseAmount: readJotFormValue(record, 184, null) || 0,
+      agreedAmount: readJotFormValue(record, 184, null) || 0,
+      effectiveDate: agreement.dateArrived || record.created_at,
+      cycle: PaymentCycle.oneoff,
+      currency: PaymentCurrency.ugx,
+      status: (readJotFormValue(record, 24, null) === 'YES') ? PaymentPlanStatus.completed : PaymentPlanStatus.active,
+      payments: []
+    });
 
-        if (readJotFormValue(record, 149, null)) {
-            kanzuPaymentPlan.payments.push(<Payment>({
-                paymentType: PaymentType.kanzu,
-                paidBy: agreement.tenant.firstName,
-                amount: readJotFormValue(record, 149, 2) || 0,
-                paidTo: readJotFormValue(record, 149, 3),
-                paymentMethod: PaymentMethod.cash,
-                status: PaymentStatus.completed
-            }));
-        }
-        return kanzuPaymentPlan;
+    if (readJotFormValue(record, 149, null)) {
+      kanzuPaymentPlan.payments.push(<Payment>({
+        paymentType: PaymentType.kanzu,
+        paidBy: agreement.tenant.firstName,
+        amount: readJotFormValue(record, 149, 2) || 0,
+        paidTo: readJotFormValue(record, 149, 3),
+        paymentMethod: PaymentMethod.cash,
+        status: PaymentStatus.completed
+      }));
     }
+    return kanzuPaymentPlan;
+  }
+  return new PaymentPlan();
 }
