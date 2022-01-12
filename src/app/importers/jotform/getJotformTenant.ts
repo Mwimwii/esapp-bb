22f07308546fc6b2a346bfbf14910ee66b2c262f
Isaac Obote <iobote@udbl.co.ug>
2022-01-12 03:46:14 +0300

    [titl-1201629851552237] getJotformTenant Resolved initilization

--- src/app/importers/jotform/getJotformTenant.ts
+++ src/app/importers/jotform/getJotformTenant.ts
@@ -9,39 +9,39 @@ import { readJotFormValue } from '../../utils/jotform/readJotFormValue';
 import { mapJotformContactDetailPhoneType } from '../../mappers/mapJotformContactDetailPhoneType';
 
 export async function getJotformTenant(record: any, repository: Repository<Contact>): Promise<Contact> {
-    let contact = await repository.findOne({ where: [{ firstName: readJotFormValue(record, 94, 'first'), lastName: readJotFormValue(record, 94, 'last'), dob: readJotFormDate(record, 165) }] });
+  let contact = await repository.findOne({ where: [{ firstName: readJotFormValue(record, 94, 'first'), lastName: readJotFormValue(record, 94, 'last'), dob: readJotFormDate(record, 165) }] });
 
-    if (!contact) {
-        contact = <Contact>({
-            firstName: readJotFormValue(record, 94, 'first'),
-            lastName: readJotFormValue(record, 94, 'last'),
-            nickName: readJotFormValue(record, 183, null),
-            gender: mapJotformGender(readJotFormValue(record, 97, null)),
-            contactType: ContactType.tenant,
-            languages: mapJotFormLanguage(readJotFormValue(record, 101, null)),
-            dob: readJotFormDate(record, 165),
-            contactDetails: []
-        });
+  if (!contact) {
+    contact = <Contact><unknown>({
+      firstName: readJotFormValue(record, 94, 'first'),
+      lastName: readJotFormValue(record, 94, 'last'),
+      nickName: readJotFormValue(record, 183, null),
+      gender: mapJotformGender(readJotFormValue(record, 97, null)),
+      contactType: ContactType.tenant,
+      languages: mapJotFormLanguage(readJotFormValue(record, 101, null)),
+      dob: readJotFormDate(record, 165),
+      contactDetails: []
+    });
 
-        if (readJotFormValue(record, 177, 1)) {
-            contact.contactDetails.push(<ContactDetail>({
-                contactDetailType: mapJotformContactDetailPhoneType(readJotFormValue(record, 177, 2)),
-                preferred: true,
-                contactDetailValue: SanitizeNumber(readJotFormValue(record, 177, 1))
-            }));
-        }
-
-        if (readJotFormValue(record, 177, 8)) {
-            contact.contactDetails.push(<ContactDetail>({
-                contactDetailType: mapJotformContactDetailPhoneType(readJotFormValue(record, 177, 5)),
-                preferred: true,
-                contactDetailValue: SanitizeNumber(readJotFormValue(record, 177, 8))
-            }));
-        }
-
-        await repository.save(contact);
+    if (readJotFormValue(record, 177, 1)) {
+      contact.contactDetails.push(<ContactDetail>({
+        contactDetailType: mapJotformContactDetailPhoneType(readJotFormValue(record, 177, 2)),
+        preferred: true,
+        contactDetailValue: SanitizeNumber(readJotFormValue(record, 177, 1))
+      }));
     }
 
-    return contact;
+    if (readJotFormValue(record, 177, 8)) {
+      contact.contactDetails.push(<ContactDetail>({
+        contactDetailType: mapJotformContactDetailPhoneType(readJotFormValue(record, 177, 5)),
+        preferred: true,
+        contactDetailValue: SanitizeNumber(readJotFormValue(record, 177, 8))
+      }));
+    }
+
+    await repository.save(contact);
+  }
+
+  return contact;
 }
 
