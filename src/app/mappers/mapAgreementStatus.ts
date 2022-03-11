99c5a154f7702f795c5f84c8b012aafe9bc6d3e1
Isaac Obote <iobote@udbl.co.ug>
2022-03-11 15:41:23 +0300

    [titl-1201952111317184] Remapped AgreementStatus for airtable

--- src/app/mappers/mapAgreementStatus.ts
+++ src/app/mappers/mapAgreementStatus.ts
@@ -1,18 +1,25 @@
 import { AgreementStatus } from '@titl-all/shared/dist/enum';
 
 export function mapAgreementStatus(val: string): AgreementStatus {
-    switch (val) {
-        case 'Agreed with landowner':
-            return AgreementStatus.negagreed;
-        case 'Been contacted':
-            return AgreementStatus.contacted;
-        case 'Missing documentation, post negotiation':
-            return AgreementStatus.negmissingdocs;
-        case 'Performed physical negotiation':
-            return AgreementStatus.negperformed;
-        case 'Planned negotiation time':
-            return AgreementStatus.negplanned;
-        default:
-            return AgreementStatus.active;
-    }
+  switch (val) {
+    case 'Been contacted, no answer':
+      return AgreementStatus.contactedfail;
+    case 'Been contacted':
+      return AgreementStatus.contactedfail;
+    case 'Planned negotiation time':
+      return AgreementStatus.negplanned;
+    case 'Performed physical negotiation':
+      return AgreementStatus.negperformed;
+    case 'Agreed with landowner':
+      return AgreementStatus.negagreed;
+    case 'Complete access to USSD':
+      return AgreementStatus.identified;
+    case 'Missing documentation, post negotiation':
+      return AgreementStatus.negmissingdocs;
+    case 'Ready For Landlord Review':
+      return AgreementStatus.negready;
+    case 'Identified':
+    default:
+      return AgreementStatus.identified
+  }
 }
