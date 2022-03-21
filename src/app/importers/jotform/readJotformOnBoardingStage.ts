f241e1f2b4d1ac02fd900ce10e5b729bbf863bbc
Isaac Obote <iobote@udbl.co.ug>
2022-03-21 08:35:46 +0300

    [titl-1201963763460899] Adds readJotformOnBoardingStage function

---
+++ src/app/importers/jotform/readJotformOnBoardingStage.ts
@@ -0,0 +1,16 @@
+
+
+export function readJotformOnBoardingStage(record: any): string | undefined {
+  const answers = Object.values(record.answers);
+  const staging: any = answers.find(({ name }: any) => {
+    return name === 'tenantonboardingstage';
+  });
+
+  if (staging) {
+    console.log(staging?.answer);
+    const lookup = JSON.parse(staging?.options_array);
+    return lookup[staging?.answer.replace(/[{}]/gm, '')].value;
+  }
+
+  return;
+}
